bb5dac0d73968368d3e5d20c73ecee0cd2b13e70
chrisheckler <hecklerchris@hotmail.com>
2016-01-15 13:23:18 -0800

    urls, views, settings updated

--- heck_site/heck_site/urls.py
+++ heck_site/heck_site/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from heck_site import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello/'. hello),
+    url(r'^hello/$', views.hello),
+    url(r'^time/$', views.current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', views.hours_ahead),
 ]
